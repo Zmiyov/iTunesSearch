97831f0af554e0591ec91fd28327f4c2840dfa12
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-06-23 11:28:13 +0300

    Initial Commit

---
+++ iTunesSearch/StoreItemController.swift
@@ -0,0 +1,32 @@
+//
+//  StoreItemController.swift
+//  iTunesSearch
+//
+//  Created by Vladimir Pisarenko on 23.06.2022.
+//
+
+import Foundation
+
+class StoreItemController {
+    
+    enum StoreItemError: Error, LocalizedError {
+        case itemsNotFound
+    }
+    
+    func fetchItems(matching query: [String: String]) async throws -> [StoreItem] {
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
+        urlComponents.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+        
+        let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw StoreItemError.itemsNotFound
+        }
+        
+        let decoder = JSONDecoder()
+        let searchResponse = try decoder.decode(SearchResponse.self, from: data)
+
+        return searchResponse.results
+    }
+    
+}
